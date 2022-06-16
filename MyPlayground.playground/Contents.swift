6a5fc657f076420e04411648e517bb955ae3c6b4
Vedant Mistry <mistryvedant17@gmail.com>
2022-06-16 13:27:15 +0530

    changes made in the starter code.

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,15 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+//assignment operators
+var a = 4
+let b = 10
+a = b
+
+//using the ternary operators in swift
+let sentence = "apple"
+var company = "apple"
+
+(sentence == company) ? "company is apple" : "company is google"
+
